ac2ca67cfbdcd85ba4bbb4c5909a4c40f157166b
Spencer Heywood <l.spencer.heywood@protonmail.com>
2022-07-27 12:52:31 -0600

    set ingress to listen on port 9080 and 9443 on Docker host

--- kind.sh
+++ kind.sh
@@ -22,7 +22,27 @@ create_network() {
 }
 
 create_cluster() {
-    kind create cluster --name=argo-apps
+
+cat <<EOF | kind create cluster --name=argo-apps --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 9080
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 9443
+    protocol: TCP
+EOF
+
     create_status=$?
    
     ## If argo-apps kind cluster was created successfully
